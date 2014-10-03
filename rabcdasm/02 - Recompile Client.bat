282a45e977196e595eab1468ae87e86b045af4f1
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-10-03 15:42:17 +0200

    Automatic Commit by BlackRayquaza

--- rabcdasm/02 - Recompile Client.bat
+++ rabcdasm/02 - Recompile Client.bat
@@ -1,4 +1,42 @@
 rabcasm client-1\client-1.main.asasm
 abcreplace client.swf 1 client-1\client-1.main.abc
+REM swfbinreplace client.swf 41 client-41.bin
 swfbinreplace client.swf 42 client-42.bin
+REM swfbinreplace client.swf 46 client-46.bin
+REM swfbinreplace client.swf 50 client-50.bin
+REM swfbinreplace client.swf 53 client-53.bin
+REM swfbinreplace client.swf 54 client-54.bin
+REM swfbinreplace client.swf 55 client-55.bin
+REM swfbinreplace client.swf 56 client-56.bin
+REM swfbinreplace client.swf 59 client-59.bin
+REM swfbinreplace client.swf 62 client-62.bin
+REM swfbinreplace client.swf 65 client-65.bin
+REM swfbinreplace client.swf 71 client-71.bin
+REM swfbinreplace client.swf 73 client-73.bin
+REM swfbinreplace client.swf 74 client-74.bin
+REM swfbinreplace client.swf 76 client-76.bin
+REM swfbinreplace client.swf 79 client-79.bin
+REM swfbinreplace client.swf 81 client-81.bin
+REM swfbinreplace client.swf 88 client-88.bin
+REM swfbinreplace client.swf 100 client-100.bin
+REM swfbinreplace client.swf 104 client-104.bin
+REM swfbinreplace client.swf 107 client-107.bin
+REM swfbinreplace client.swf 108 client-108.bin
+REM swfbinreplace client.swf 110 client-110.bin
+REM swfbinreplace client.swf 113 client-113.bin
+REM swfbinreplace client.swf 119 client-119.bin
+REM swfbinreplace client.swf 123 client-123.bin
+REM swfbinreplace client.swf 124 client-124.bin
+REM swfbinreplace client.swf 125 client-125.bin
+REM swfbinreplace client.swf 126 client-126.bin
+REM swfbinreplace client.swf 130 client-130.bin
+REM swfbinreplace client.swf 134 client-134.bin
+REM swfbinreplace client.swf 141 client-141.bin
+REM swfbinreplace client.swf 146 client-146.bin
+REM swfbinreplace client.swf 150 client-150.bin
+REM swfbinreplace client.swf 153 client-153.bin
+REM swfbinreplace client.swf 156 client-156.bin
+REM swfbinreplace client.swf 161 client-161.bin
+REM swfbinreplace client.swf 163 client-163.bin
+REM swfbinreplace client.swf 169 client-169.bin
 pause
