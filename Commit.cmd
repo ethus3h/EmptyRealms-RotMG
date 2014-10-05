388ce2d4621c42ef05d3749b7b8a0f873d0a8956
BlackRayquaza <shadowrealmrotmg@gmail.com>
2014-10-05 21:42:04 +0200

    Automatic Commit by BlackRayquaza

--- Commit.cmd
+++ Commit.cmd
@@ -1,7 +1,7 @@
 git init
 git add --all --force
 git commit -m "Automatic Commit by BlackRayquaza"
-git remote add https://github.com/OpenRealms/EmptyRealms-RotMG.git
+git remote add https://BlackRayquaza@bitbucket.org/BlackRayquaza/emptyrealms-rotmg.git
 git push -u origin master
 pause
 REM Cute :3
